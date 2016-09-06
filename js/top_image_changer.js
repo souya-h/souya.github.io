4a8f8762700ed4a9ad22310c08b4c7cdefaa7c59
Souya H <souya.handa@gmail.com>
2016-09-07 01:29:17 +0900

    cleared erroring js

--- js/top_image_changer.js
+++ js/top_image_changer.js
@@ -1,15 +1,3 @@
 // JavaScript Document
-window.onload = function(){
-var folderPath = "/arts/img/lost_property_series"
-folderRef = new Folder (folderPath);	//select a folder to get file names
-fileList = folderRef.getFiles();	//get the list of files
-var filePath = [];
-for (i=0; i<fileList.length; i++)	
-{
-filePath[i] = fileList[i].fullName;	
-}
-var rand = Math.floor( Math.random() * fileList.length ) ;
-$('#top_image').attr('src', filePath[rand])
-}
-//$(window).load(){}
+
 	
