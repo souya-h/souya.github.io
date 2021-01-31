3f0918e04b81d358fc626291b9de401392c1dfc5
unknown <souya.handa@gmail.com>
2021-02-01 06:57:22 +0900

    add files of nanjin

---
+++ war_of_historization/js/war_of_historization.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+$(document).ready(function() {
+
+});
+$(window).on("load", function(){
+  // Handler when all assets (including images) are loaded
+  $('#iframe_lgmd').contents().find('div').append('<em>★デュフフ<\/em>');
+});
