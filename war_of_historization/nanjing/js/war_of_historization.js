44e1bed745bf5db200cfc4478c96192fc97d7a27
unknown <souya.handa@gmail.com>
2021-02-01 09:57:23 +0900

    tuned sequence

--- war_of_historization/nanjing/js/war_of_historization.js
+++ war_of_historization/nanjing/js/war_of_historization.js
@@ -9,12 +9,12 @@ window.addEventListener("touchstart", function (event) {
 
 
   if(touchCount>5){
-    document.body.innerHTML = document.body.innerHTML.replace("屠殺", "処理");
+    document.body.innerHTML = document.body.innerHTML.replace("南京", "東京");
     document.body.innerHTML = document.body.innerHTML.replace("略奪", "募金");
     document.body.innerHTML = document.body.innerHTML.replace("虐殺", "事件");
   }
   if(touchCount>10){
-    document.body.innerHTML = document.body.innerHTML.replace("南京", "東京");
+    document.body.innerHTML = document.body.innerHTML.replace("屠殺", "処理");
     document.body.innerHTML = document.body.innerHTML.replace("暴行", "指導");
     document.body.innerHTML = document.body.innerHTML.replace("殺人", "罰則");
     document.body.innerHTML = document.body.innerHTML.replace("虐殺", "事件");
