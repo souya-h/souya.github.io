a799a2e300a885b45227549fc71ed384edd955dc
unknown <souya.handa@gmail.com>
2021-02-01 09:25:43 +0900

    tuned replacing frequency

--- war_of_historization/nanjing/js/war_of_historization.js
+++ war_of_historization/nanjing/js/war_of_historization.js
@@ -11,13 +11,15 @@ window.addEventListener("touchstart", function (event) {
   if(touchCount>5){
     document.body.innerHTML = document.body.innerHTML.replace("屠殺", "処理");
     document.body.innerHTML = document.body.innerHTML.replace("略奪", "募金");
+    document.body.innerHTML = document.body.innerHTML.replace("虐殺", "事件");
   }
   if(touchCount>10){
     document.body.innerHTML = document.body.innerHTML.replace("南京", "東京");
     document.body.innerHTML = document.body.innerHTML.replace("暴行", "指導");
     document.body.innerHTML = document.body.innerHTML.replace("殺人", "罰則");
+    document.body.innerHTML = document.body.innerHTML.replace("虐殺", "事件");
   }
-  if(touchCount>15){
+  if(touchCount>20){
     $('main').prepend('<img src="./src/kyokujitsuki.png" width="50%">');
     document.body.innerHTML = document.body.innerHTML.replace("日本軍", "中国軍");
   }
