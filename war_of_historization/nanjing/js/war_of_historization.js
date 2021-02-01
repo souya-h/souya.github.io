5b18c07400af89133b23ebacd681c0883a639553
unknown <souya.handa@gmail.com>
2021-02-01 09:02:21 +0900

    updated js about ontouchstart

--- war_of_historization/nanjing/js/war_of_historization.js
+++ war_of_historization/nanjing/js/war_of_historization.js
@@ -1,20 +1,21 @@
 // JavaScript Document
-var scrollCount = 0;
-$(window).touchStart(function() {
+var touchCount = 0;
+var clickEventType=((window.ontouchstart!==null)?'click':'touchstart');
+$("#test").on(clickEventType, function() {
   // windowがスクロールされた時に実行する処理
   document.body.innerHTML = document.body.innerHTML.replace("虐殺", "事件");
 
-  scrollCount++;
-  if(scrollCount>5){
+  touchCount++;
+  if(touchCount>5){
     document.body.innerHTML = document.body.innerHTML.replace("屠殺", "処理");
     document.body.innerHTML = document.body.innerHTML.replace("略奪", "募金");
   }
-  if(scrollCount>10){
+  if(touchCount>10){
     document.body.innerHTML = document.body.innerHTML.replace("南京", "東京");
     document.body.innerHTML = document.body.innerHTML.replace("暴行", "指導");
     document.body.innerHTML = document.body.innerHTML.replace("殺人", "罰則");
   }
-  if(scrollCount>15){
+  if(touchCount>15){
     $('main').prepend('<img src="./src/kyokujitsuki.png" width="50%">');
     document.body.innerHTML = document.body.innerHTML.replace("日本軍", "中国軍");
   }
